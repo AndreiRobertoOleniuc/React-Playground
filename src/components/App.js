a24ec3827d1c5095b10352a2c170b3e694eac732
Andrei Oleniuc <andreipsn14@gmail.com>
2020-08-29 16:43:00 +0200

    Bootstrap implementation done

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Footer from "./Footer";
+import "bootstrap/dist/css/bootstrap.css";
+import { Navbar, NavDropdown, Nav } from "react-bootstrap";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Navbar from "./Navbar";
-import "bootstrap/dist/css/bootstrap.css";
+import App from "./components/App";
 
-ReactDOM.render(<Navbar />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 /*<React.StrictMode>
     <App />
